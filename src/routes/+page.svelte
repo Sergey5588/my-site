58752a29719714c2f4e68b529ed554fc998897f6
Sergey5588 <sergey271109@gmail.com>
2025-04-14 21:33:01 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,86 @@
+
+
+
+
+<style global>
+	* {
+		font-family: "Inter", sans-serif !important;
+	}
+	
+	:root {
+		--bg-color: #121619;
+		--font-color: #F7FFF6;
+	}
+	
+	:global(body) {
+		
+		background-color: var(--bg-color);
+		color: var(--font-color);
+	}
+
+
+
+
+	:global(button) {
+		padding: 10px 20px;
+		font-size: 16px;
+		background-color: #63C132;
+		color: var(--font-color);
+		border: none;
+		border-radius: 8px;
+		transition: transform 0.2s ease, box-shadow 0.2s ease;
+		cursor: pointer;
+	}
+
+	/* Увеличение при hover */
+	button:hover {
+		transform: scale(1.1);
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+	}
+
+	/* Уменьшение при нажатии */
+	button:active {
+		transform: scale(0.95);
+	}
+
+</style>
+
+
+
+<script>
+	export const prerender = true;
+	let numbers = $state([1, 2, 3, 4]);
+	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+	let items = $state([1,2,3]);
+	let pinned = $state();
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		
+		items.push(items.length+1);
+	}
+
+	async function get_repo() {
+		const url = "https://pinned.berrysauce.dev/get/sergey5588";
+		fetch(url).then(response=>response.json()).then(data=>pinned=data);
+	}
+</script>
+
+
+<h1>{numbers.join(' + ')} = {total}</h1>
+
+<button onclick={addNumber}>
+	Add a number<br>hi 123
+</button>
+<br>
+<br>
+<button onclick={get_repo}>
+	Get repo
+</button>
+
+{#each pinned as i}
+	<h1>{i.name}</h1>
+{/each}
+
+
+
+
