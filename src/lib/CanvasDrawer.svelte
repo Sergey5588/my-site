6c35f6190811969d47ec9cb61fdf7cb4ebc385c8
Sergey5588 <sergey271109@gmail.com>
2025-04-21 20:59:39 +0300

    test

---
+++ src/lib/CanvasDrawer.svelte
@@ -0,0 +1,53 @@
+
+<script>
+    import { onMount } from 'svelte';
+    
+    //export let color = '#000000';
+
+    let canvas;
+    let ctx;
+    let text = 'Hello';
+
+
+    const chars = "01日ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const fontSize = 64;
+   
+            
+    const drops = [];
+    
+    onMount(() => {
+        ctx = canvas.getContext('2d');
+        resize()
+        window.addEventListener('resize', resize);
+        
+    });
+
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    
+    
+    
+    
+</script>
+
+<canvas
+    tabindex="-1"
+    bind:this={canvas}
+    class="canvas"
+></canvas>
+<style>
+    .canvas {
+        
+        border: 1px solid white;
+        pointer-events: none;
+        -webkit-user-drag: none;
+        position: absolute;
+        top:0px;
+        left: 0px;
+        z-index: -1;
+    }
+    
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,51 +3,59 @@
 
 
 <style global>
-	* {
-		font-family: "Inter", sans-serif !important;
-	}
-	
-	:root {
-		--bg-color: #121619;
-		--font-color: #F7FFF6;
-	}
-	
-	:global(body) {
+	:global {
+			* {
+			font-family: "Inter", sans-serif !important;
+		}
 		
-		background-color: var(--bg-color);
-		color: var(--font-color);
+		:root {
+			--bg-color: #121619;
+			--font-color: #F7FFF6;
+			--first-color: #63C132;
+			--second-color: #19310D;
+			--third-color: #DEF3D2;
+		}
+		
+		body {
+			
+			background-color: var(--bg-color);
+			color: var(--font-color);
+		}
+
+
+
+
+		button {
+			padding: 10px 20px;
+			font-size: 16px;
+			background-color: var(--first-color);
+			color: var(--font-color);
+			border: none;
+			border-radius: 8px;
+			transition: transform 0.2s ease, box-shadow 0.2s ease;
+			cursor: pointer;
+		}
+
+		/* Увеличение при hover */
+		button:hover {
+			transform: scale(1.1);
+			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+		}
+
+		/* Уменьшение при нажатии */
+		button:active {
+			transform: scale(0.95);
+		}
 	}
-
-
-
-
-	:global(button) {
-		padding: 10px 20px;
-		font-size: 16px;
-		background-color: #63C132;
-		color: var(--font-color);
-		border: none;
-		border-radius: 8px;
-		transition: transform 0.2s ease, box-shadow 0.2s ease;
-		cursor: pointer;
-	}
-
-	/* Увеличение при hover */
-	button:hover {
-		transform: scale(1.1);
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-	}
-
-	/* Уменьшение при нажатии */
-	button:active {
-		transform: scale(0.95);
-	}
+	
+	
 
 </style>
 
 
 
 <script>
+	import CanvasDrawer from '../lib/CanvasDrawer.svelte';
 	let numbers = $state([1, 2, 3, 4]);
 	let total = $derived(numbers.reduce((t, n) => t + n, 0));
 	let items = $state([1,2,3]);
