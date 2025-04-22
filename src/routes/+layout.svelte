0fdb7e89b0ed1a50ac2ef6b9bd2c47142b34f4f6
Sergey5588 <sergey271109@gmail.com>
2025-04-22 21:22:30 +0300

    moved css to separate file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+
+
+<style>
+    @import '../main.css';
+</style>
+
+<script>
+    
+    let {children} =$props();
+</script>
+
+<h1>
+    global text!!
+</h1>
+
+
+
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,7 @@
 
 
-
-
-<style global>
-	:global {
-			* {
-			font-family: "Inter", sans-serif !important;
-		}
-		
-		:root {
-			--bg-color: #121619;
-			--font-color: #F7FFF6;
-			--first-color: #63C132;
-			--second-color: #19310D;
-			--third-color: #DEF3D2;
-		}
-		
-		body {
-			
-			background-color: var(--bg-color);
-			color: var(--font-color);
-		}
-
-
-
-
-		button {
-			padding: 10px 20px;
-			font-size: 16px;
-			background-color: var(--first-color);
-			color: var(--font-color);
-			border: none;
-			border-radius: 8px;
-			transition: transform 0.2s ease, box-shadow 0.2s ease;
-			cursor: pointer;
-		}
-
-		/* Увеличение при hover */
-		button:hover {
-			transform: scale(1.1);
-			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-		}
-
-		/* Уменьшение при нажатии */
-		button:active {
-			transform: scale(0.95);
-		}
-	}
-	
-	
-
-</style>
-
-
-
 <script>
-	import CanvasDrawer from '../lib/CanvasDrawer.svelte';
+	import CanvasDrawer from '$lib/CanvasDrawer.svelte';
 	let numbers = $state([1, 2, 3, 4]);
 	let total = $derived(numbers.reduce((t, n) => t + n, 0));
 	let items = $state([1,2,3]);
@@ -70,6 +16,8 @@
 		const url = "https://pinned.berrysauce.dev/get/sergey5588";
 		fetch(url).then(response=>response.json()).then(data=>pinned=data);
 	}
+
+	
 </script>
 
 
@@ -86,9 +34,7 @@
 
 {#each pinned as i}
 	<h1>{i.name}</h1>
-	<button>{i.language}</button>
+	<button onclick={window.open(`https://github.com/${i.author}/${i.name}`)}>{i.language}</button>
 {/each}
-
-
 
 
