7689298e99d53a89c649f312d40ef3c0fe46fdff
Sergey5588 <sergey271109@gmail.com>
2025-05-01 00:22:04 +0300

    new tool: calculator + styles

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Nothing here...</h1>
+<script>
+    
+    var result = $state("")
+    var one = $state("")
+    var two = $state("")
+    function add() {
+        result = one+two
+    }
+</script>
+
+<h1>Calculator</h1>
+<input bind:value = {one} type="text" placeholder="First number"/>
+<input bind:value = {two} type="text" placeholder="Second number"/>
+<button onclick={add}>Calculate!</button>
+
+<h1>Result: {result}</h1>
